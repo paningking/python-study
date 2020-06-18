1b90bd579183d76d09166147120d2432b9bd7472
paningking <paningking@gmail.com>
2020-06-18 18:27:19 +0800

    初始化

--- reactive-python/src/reactive/rxdemo.py
+++ reactive-python/src/reactive/rxdemo.py
@@ -6,6 +6,6 @@ b = rx.of(2, 2, 4, 4)
 
 a.pipe(
     ops.zip(b),  # returns a tuple with the items of a and b
-    ops.map(lambda z: operator.mul(z[0], z[1]))
+    ops.map(lambda z: operator.mul(z[0], z[1])),
     ops.flat_map(lambda x: rx.range(0, x))
 ).subscribe(print)
